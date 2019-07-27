3e0e1c63ea9b3372efe2db382d6e7c8d266ed6dc
Henning Jacobs <henning@zalando.de>
2019-07-27 16:41:06 +0200

    Install K3s

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # K3s Demo Server
 
 Single-node K3s server showing some Open Source Kubernetes tools.
+
+## Installation
+
+Base OS: Ubuntu 18.04
+
+```
+apt install git
+git clone https://codeberg.org/hjacobs/k3s-demo.git
+cd k3s-demo
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install K3s
+curl -sfL https://get.k3s.io | sh -
+
+# Install Metrics Server
+git clone git@github.com:kubernetes-incubator/metrics-server.git
+kubectl apply -f metrics-server/deploy/1.8+/
+kubectl top node
+
+git clone https://github.com/hjacobs/kube-ops-view.git
+kubectl apply -f kube-ops-view/deploy/
